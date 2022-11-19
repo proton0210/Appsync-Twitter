3b5029f43465b8cfb5cf56ea7bb6365814bc0eb6
Vidit Shah <vidit0210@protonmail.com>
2022-11-20 01:17:02 +0530

    Schema Version One Complete

--- backend/lib/stacks/API_STACK/api-stack.ts
+++ backend/lib/stacks/API_STACK/api-stack.ts
@@ -1,8 +1,9 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 
+interface ApiStackProps extends cdk.StackProps {}
 export class ApiStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: ApiStackProps) {
     super(scope, id, props);
   }
 }
