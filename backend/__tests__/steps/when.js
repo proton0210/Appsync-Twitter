37c1dd943f0dfd933cef0c7994930f9b0d7b7d39
Vidit Shah <vidit0210@protonmail.com>
2022-11-29 22:23:17 +0530

    e2e for timeline

--- backend/__tests__/steps/when.js
+++ backend/__tests__/steps/when.js
@@ -61,6 +61,34 @@ const we_invoke_getImageUploadUrl = async (
 
   return await handler(event, context);
 };
+
+const a_user_calls_tweet = async (user, text) => {
+  const tweet = `mutation tweet($text: String!) {
+    tweet(text: $text) {
+      id
+      createdAt
+      text
+      replies
+      likes
+      retweets
+    }
+  }`;
+  const variables = {
+    text
+  };
+
+  const data = await GraphQL(
+    process.env.API_URL,
+    tweet,
+    variables,
+    user.accessToken
+  );
+  const newTweet = data.tweet;
+
+  console.log(`[${user.username}] - posted new tweet`);
+
+  return newTweet;
+};
 const we_invoke_tweet = async (username, text) => {
   const handler =
     require('../../lib/stacks/API_STACK/resolvers/mutations/Tweet/index').handler;
@@ -228,5 +256,6 @@ module.exports = {
   a_user_calls_editMyProfile,
   we_invoke_getImageUploadUrl,
   a_user_calls_getImageUploadUrl,
-  we_invoke_tweet
+  we_invoke_tweet,
+  a_user_calls_tweet
 };
