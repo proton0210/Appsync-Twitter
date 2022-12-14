79bad57742895f89471473e77ed46cc4285a6033
Vidit Shah <vidit0210@protonmail.com>
2022-12-14 22:20:05 +0530

    Refactor of Frontend Setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
-import * as React from 'react'
-const App:React.FC = () => {
+import React from 'react';
+
+function App() {
   return (
     <div>
-      <h1>React App</h1>
+      <h1>Hello</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
